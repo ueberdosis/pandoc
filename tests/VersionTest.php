1a987adeb0a6f7eda6c447941e0b1a760806afec
Hans Pagel <hans.pagel@ueber.io>
2020-03-17 18:59:03 +0100

    add debug output

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -12,6 +12,9 @@ class VersionTest extends TestCase
     {
         $output = (new Pandoc)->version();
 
+        var_dump($output);
+        exit();
+
         $this->assertTrue(version_compare($output, '2.0.0', '>='));
         $this->assertTrue(version_compare($output, '3.0.0', '<'));
     }
