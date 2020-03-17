e1a562f0176f88630f9271fa11f6c2a47c8f3769
Hans Pagel <hans.pagel@ueber.io>
2020-03-17 19:24:24 +0100

    fix everything

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -10,10 +10,7 @@ class VersionTest extends TestCase
     /** @test */
     public function pandoc_returns_version()
     {
-        $output = (new Pandoc)->version(true);
-
-        var_dump($output);
-        exit();
+        $output = (new Pandoc)->version();
 
         $this->assertTrue(version_compare($output, '2.0.0', '>='));
         $this->assertTrue(version_compare($output, '3.0.0', '<'));
