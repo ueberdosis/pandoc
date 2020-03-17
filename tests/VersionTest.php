9d1f2c97b9b8bbbe06937e00b08e1e0d8b75cadc
Hans Pagel <hans.pagel@ueber.io>
2020-03-17 18:40:48 +0100

    init

---
+++ tests/VersionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ueberdosis\Pandoc\Tests;
+
+use Ueberdosis\Pandoc\Pandoc;
+use PHPUnit\Framework\TestCase;
+
+class VersionTest extends TestCase
+{
+    /** @test */
+    public function pandoc_returns_version()
+    {
+        $output = (new Pandoc)->version();
+
+        $this->assertTrue(version_compare($output, '2.0.0', '>='));
+        $this->assertTrue(version_compare($output, '3.0.0', '<'));
+    }
+}
